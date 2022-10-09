2aa13494aa86c1d1f3b0e6ca2741a24458604727
ibadil307 <43997372+ibadil307@users.noreply.github.com>
2022-10-10 01:58:09 +0800

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,7 @@ function Home() {
         <div className="container">
           <div className="text-wrapper">
             <h1 className="title">Halo, saya Muhammad Ibadil Mukhlasin</h1>
-            <p className="description">Saya seorang pekerja lepas pengembang web, fotograper, desainer antarmuka pengguna, dan penulis.</p>
+            <p className="description">Saya seorang manusia.</p>
 
             <Link href="/contact"><a className="cta">Kontak Saya</a></Link>
           </div>
