8315346dc8b812e67cebaa522d47166fa310bcb8
ibadil307 <43997372+ibadil307@users.noreply.github.com>
2022-10-10 09:55:40 +0800

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,7 @@ function Home() {
       <Head>
         <title>Home</title>
       </Head>
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
       <Navbar />
 
       <section className="hero">
