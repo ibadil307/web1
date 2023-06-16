b2634638bf2f89c27e4118be10a472288c3c8c90
ibadil307 <43997372+ibadil307@users.noreply.github.com>
2023-06-16 10:46:37 +0800

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,7 +10,7 @@ function Home() {
 
         <title>Home</title>
       </Head>
-
+document.getElementById("viewport").setAttribute("content", "width=1024, initial-scale=0, maximum-scale=1.0, minimum-scale=0.25, user-scalable=yes");
       <Navbar />
 
       <section className="hero">
